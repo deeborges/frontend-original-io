3e37edb6eb14e928f98db14f8ec1c9bd844d0108
deeborges <deyvisonsilvaborges@gmail.com>
2020-11-01 14:43:43 -0300

    fix(stop): parei o projeto

--- src/lib/components/thumbs/index.js
+++ src/lib/components/thumbs/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Container, Content, Seta } from './styled';
 import { ThumbImage } from './images';
+
+import { Video } from './video';
+
 import { IconArrowTop, IconArrowDown } from '../../../assets/icons/setas';
 
 const Thumbs = () => {
-  // const onClickSetaSubir = () => {
-  //   window.pageXOffset = window.scrollTo({ top: 20, behavior: 'smooth' });
-
-  //   console.log(window.pageXOffset);
-  // };
   return (
     <Container>
+      <Video
+        src={'https://www.youtube.com/watch?v=TEzqeb719pk'}
+        width={'96px'}
+        height={'82px'}
+      />
       <Seta src={IconArrowTop} />
       <Content>
         <ThumbImage src={'https://imgur.com/vLT0BhC.png'} />
